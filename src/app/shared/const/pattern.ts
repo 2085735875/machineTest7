7be45ca46e31811e28ea47a7d78fb7b6e641c8f2
2085735875 <rupeshdesale79@gmail.com>
2024-07-08 14:58:18 +0530

    work on task

--- src/app/shared/const/pattern.ts
+++ src/app/shared/const/pattern.ts
@@ -1,7 +1,7 @@
 export class CustomRegex {
     public static password = `^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{7,}$`;
     public static onlyText = '[a-zA-Z]*';
-    public static num = '[0-9]';
+    public static num = '[0-9]*';
     public static username = '^[a-zA-Z ]*$';
     public static email = '^[a-zA-Z0-9._-]+@[a-zA-Z0-9-_.]+\\.[a-zA-Z]{2,100}$';
     public static updateEmail =
